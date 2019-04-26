8981f7c91ab3d0875db5a3fc542d0fa3a73e0713
Loren 🤓 <lorensr@gmail.com>
2019-04-26 00:28:53 -0400

    Add reviews Query and createReview Mutation

--- src/index.js
+++ src/index.js
@@ -1,14 +1,47 @@
 import { ApolloServer, gql } from 'apollo-server'
 
+const reviews = [
+  {
+    text: 'Super-duper book.',
+    stars: 5
+  }
+]
+
 const server = new ApolloServer({
   typeDefs: gql`
     type Query {
       hello: String!
+      reviews: [Review!]!
+    }
+    type Review {
+      text: String!
+      stars: Int
+      fullReview: String!
+    }
+    type Mutation {
+      createReview(review: CreateReviewInput!): Review
+    }
+    input CreateReviewInput {
+      text: String!
+      stars: Int
     }
   `,
   resolvers: {
     Query: {
-      hello: () => '🌍🌏🌎'
+      hello: () => '🌍🌏🌎',
+      reviews: () => reviews
+    },
+    Review: {
+      fullReview: review =>
+        `Someone on the internet gave ${review.stars} stars, saying: "${
+          review.text
+        }"`
+    },
+    Mutation: {
+      createReview: (_, { review }) => {
+        reviews.push(review)
+        return review
+      }
     }
   }
 })
