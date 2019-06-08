537aacc796f0719120e88df03fb4ba77f2f5eb4a
Loren 🤓 <lorensr@gmail.com>
2019-06-07 23:58:54 -0400

    Add Query.user and User.email resolver

--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -1,11 +1,21 @@
-import { AuthenticationError } from 'apollo-server'
+import { AuthenticationError, ForbiddenError } from 'apollo-server'
+import { ObjectId } from 'mongodb'
 
 export default {
   Query: {
-    me: (_, __, context) => context.user
+    me: (_, __, context) => context.user,
+    user: (_, { id }, { dataSources }) =>
+      dataSources.users.findOneById(ObjectId(id))
   },
   User: {
     id: ({ _id }) => _id,
+    email(user, _, { user: currentUser }) {
+      if (!currentUser || !user._id.equals(currentUser._id)) {
+        throw new ForbiddenError(`cannot access others' emails`)
+      }
+
+      return user.email
+    },
     photo(user) {
       // user.authId: 'github|1615'
       const githubId = user.authId.split('|')[1]
