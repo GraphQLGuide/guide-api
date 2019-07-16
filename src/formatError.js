5be6f9b6f9ff718716b798e99479f8bf8414314f
Loren 🤓 <lorensr@gmail.com>
2019-07-16 03:29:22 -0400

    Mask MongoDB errors

--- src/formatError.js
+++ src/formatError.js
@@ -1,7 +1,19 @@
 import get from 'lodash/get'
 
+const inProduction = process.env.NODE_ENV === 'production'
+
 export default error => {
-  console.log(error)
-  console.log(get(error, 'extensions.exception.stacktrace'))
-  return error
+  if (inProduction) {
+    // send error to tracking service
+  } else {
+    console.log(error)
+    console.log(get(error, 'extensions.exception.stacktrace'))
+  }
+
+  const name = get(error, 'extensions.exception.name') || ''
+  if (name.startsWith('Mongo')) {
+    return new Error('Internal server error')
+  } else {
+    return error
+  }
 }
