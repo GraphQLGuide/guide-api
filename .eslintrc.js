0b4f773e1ecb2b2d72b9d91e085b7a650626a03b
Loren 🤓 <lorensr@gmail.com>
2019-09-02 03:32:02 -0400

    Set up linting

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true
+  },
+  extends: 'plugin:node/recommended',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  rules: {
+    'node/no-unsupported-features/es-syntax': 0
+  }
+}
