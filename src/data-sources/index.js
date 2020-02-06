8d4315cf9b20a58355738e97f4a64809dbf506a0
Loren 🤓 <lorensr@gmail.com>
2020-02-06 18:03:24 -0500

    Add PPP REST data source

--- src/data-sources/index.js
+++ src/data-sources/index.js
@@ -1,11 +1,13 @@
 import Reviews from './Reviews'
 import Users from './Users'
 import Github from './Github'
+import PPP from './PPP'
 import { db } from '../db'
 
 export default () => ({
   reviews: new Reviews(db.collection('reviews')),
-  users: new Users(db.collection('users'))
+  users: new Users(db.collection('users')),
+  ppp: new PPP()
 })
 
-export { Reviews, Users, Github }
+export { Reviews, Users, Github, PPP }
